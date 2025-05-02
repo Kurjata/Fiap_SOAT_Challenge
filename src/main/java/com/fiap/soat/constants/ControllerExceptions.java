5fa7475c79e31cf9aae1791fe35968f9718e28e9
Geraldo Francisco Neto <geraldof.neto2016@gmail.com>
2025-05-01 21:02:11 -0300

    ajustado exceções de adição de produto no pedido

--- src/main/java/com/fiap/soat/constants/ControllerExceptions.java
+++ src/main/java/com/fiap/soat/constants/ControllerExceptions.java
@@ -8,7 +8,7 @@ import static lombok.AccessLevel.PRIVATE;
 public class ControllerExceptions {
 
     public static final String DOCUMENT_NUMBER_INVALID = "DOCUMENT_NUMBER_INVALID";
-    public static final String GENERIC_FIELD_REQUIRED = "GENERIC_FIELD_REQUIRED";
+
 
     //Customer
     public static final String CUSTOMER_CREATE_DOCUMENT_NUMBER_REQUIRED = "CUSTOMER_CREATE_DOCUMENT_NUMBER_REQUIRED";
@@ -22,4 +22,8 @@ public class ControllerExceptions {
     public static final String PRODUCT_CREATE_CATEGORY_INVALID = "PRODUCT_CREATE_CATEGORY_INVALID";
     public static final String PRODUCT_CREATE_AMOUNT_REQUIRED = "PRODUCT_CREATE_AMOUNT_REQUIRED";
 
+    //ORDER
+    public static final String ORDER_ADD_PRODUCT_ORDER_ID_REQUIRED = "ORDER_ADD_PRODUCT_ORDER_ID_REQUIRED";
+    public static final String ORDER_ADD_PRODUCT_PRODUCT_ID_REQUIRED = "ORDER_ADD_PRODUCT_PRODUCT_ID_REQUIRED";
+    public static final String ORDER_ADD_PRODUCT_QUANTITY_REQUIRED = "ORDER_ADD_PRODUCT_QUANTITY_REQUIRED";
 }
--- src/main/java/com/fiap/soat/model/request/order/OrderAddProductRequest.java
+++ src/main/java/com/fiap/soat/model/request/order/OrderAddProductRequest.java
@@ -1,5 +1,14 @@
 package com.fiap.soat.model.request.order;
 
+import static com.fiap.soat.constants.ControllerExceptions.ORDER_ADD_PRODUCT_ORDER_ID_REQUIRED;
+import static com.fiap.soat.constants.ControllerExceptions.ORDER_ADD_PRODUCT_PRODUCT_ID_REQUIRED;
+import static com.fiap.soat.constants.ControllerExceptions.ORDER_ADD_PRODUCT_QUANTITY_REQUIRED;
+import static com.fiap.soat.constants.Description.ORDER_ID_DESCRIPTION;
+import static com.fiap.soat.constants.Description.ORDER_PRODUCT_QUANTITY_DESCRIPTION;
+import static com.fiap.soat.constants.Description.PRODUCT_ID_DESCRIPTION;
+import static com.fiap.soat.constants.Example.ID_EXAMPLE;
+import static com.fiap.soat.constants.Example.INTEGER_EXAMPLE;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -8,27 +17,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import static com.fiap.soat.constants.ControllerExceptions.GENERIC_FIELD_REQUIRED;
-import static com.fiap.soat.constants.Description.ORDER_ID_DESCRIPTION;
-import static com.fiap.soat.constants.Description.ORDER_PRODUCT_QUANTITY_DESCRIPTION;
-import static com.fiap.soat.constants.Description.PRODUCT_ID_DESCRIPTION;
-import static com.fiap.soat.constants.Example.ID_EXAMPLE;
-import static com.fiap.soat.constants.Example.INTEGER_EXAMPLE;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderAddProductRequest {
-  @NotBlank(message = GENERIC_FIELD_REQUIRED)
+  @NotBlank(message = ORDER_ADD_PRODUCT_ORDER_ID_REQUIRED)
   @Schema(description = ORDER_ID_DESCRIPTION, example = ID_EXAMPLE)
   private String orderId;
 
-  @NotBlank(message = GENERIC_FIELD_REQUIRED)
+  @NotBlank(message = ORDER_ADD_PRODUCT_PRODUCT_ID_REQUIRED)
   @Schema(description = PRODUCT_ID_DESCRIPTION, example = ID_EXAMPLE)
   private String productId;
 
-  @NotNull(message = GENERIC_FIELD_REQUIRED)
+  @NotNull(message = ORDER_ADD_PRODUCT_QUANTITY_REQUIRED)
   @Schema(description = ORDER_PRODUCT_QUANTITY_DESCRIPTION, example = INTEGER_EXAMPLE)
   private Integer quantity;
 }
